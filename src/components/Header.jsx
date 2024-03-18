befafec6ffb6d39fb8a4c032233b06849c2a8b5e
Vincent Hertz <vincent.hertz@gmail.com>
2024-03-19 00:47:00 +0100

    codage Header (jsx +scss)

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/Header.scss";
+
+function Header() {
+  return (
+    <header>
+      <ul>
+        <li>About me</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
