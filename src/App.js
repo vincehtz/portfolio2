3dac02a2738b7f15ef1f863f4ed11ed897edf2bb
Vincent Hertz <vincent.hertz@gmail.com>
2024-03-19 00:45:46 +0100

    link fichier scss

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Error from "./pages/Error";
+import "./styles/App.scss";
 
 function App() {
   return (
