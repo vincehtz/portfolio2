59fead104d7b1121e706164c1f581cbebb283f3c
Vincent Hertz <vincent.hertz@gmail.com>
2024-03-20 00:44:10 +0100

    MAJ et traduction composant About

---
+++ src/components/SectionAbout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/SectionAbout.scss";
+
+function SectionAbout() {
+  return (
+    <section className="section__about">
+      <h2>A propos de moi</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Platea dictumst
+        vestibulum rhoncus est pellentesque elit ullamcorper. Nunc id cursus
+        metus aliquam eleifend mi. Sagittis eu volutpat odio facilisis. Vel quam
+        elementum pulvinar etiam non quam lacus.
+      </p>
+    </section>
+  );
+}
+
+export default SectionAbout;
