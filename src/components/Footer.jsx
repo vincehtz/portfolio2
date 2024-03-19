cb0816c1f844673d7534a0db0581d33edefcaa95
Vincent Hertz <vincent.hertz@gmail.com>
2024-03-20 00:42:50 +0100

    Maj Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Footer.scss";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Réalisé par Vincent Hertz </p>
+    </footer>
+  );
+}
+
+export default Footer;
