11610b635e00f3f8513cea9f7f61bef18a93148e
Vincent Hertz <vincent.hertz@gmail.com>
2024-03-20 17:15:35 +0100

    Ajout liens de redirection

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import "../styles/Button.scss";
 
-function Button({ txt }) {
-  return <button>{txt}</button>;
+function Button({ txt, link }) {
+  return (
+    <a href={link} target="_blank" rel="noreferrer">
+      <button>{txt}</button>
+    </a>
+  );
 }
 
 Button.propTypes = {
   txt: PropTypes.string,
+  link: PropTypes.string,
 };
 
 export default Button;
