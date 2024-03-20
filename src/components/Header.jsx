50b2feb7b1594a0d5ca2444cc8fdc551104cee3b
Vincent Hertz <vincent.hertz@gmail.com>
2024-03-20 17:16:02 +0100

    Ajout btn VH et DarkMode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "../styles/Header.scss";
+import Button from "./Button";
+import DarkMode from "./DarkMode";
 
 function Header() {
   return (
     <header>
+      <Button txt="VH" />
       <ul>
         <li>À propos</li>
         <li>Compétences</li>
         <li>Projets</li>
         <li>Contact</li>
       </ul>
+      <DarkMode />
     </header>
   );
 }
