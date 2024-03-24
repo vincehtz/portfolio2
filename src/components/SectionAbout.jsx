73255dc3ee64b0e2278236c9173c58b21cac9813
Vincent Hertz <vincent.hertz@gmail.com>
2024-03-24 20:01:23 +0100

    test

--- src/components/SectionAbout.jsx
+++ src/components/SectionAbout.jsx
@@ -9,7 +9,7 @@ function SectionAbout() {
         Je suis un développeur web autodidacte avec une solide expérience dans
         la conception et le développement de sites web et dapplications web
         interactives. Ma passion pour le codage a commencé dès mon plus jeune
-        âge
+        âge test
       </p>
     </section>
   );
