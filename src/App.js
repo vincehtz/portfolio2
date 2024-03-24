4b83ee475a30347b18d9b6632881519d81039407
Vincent Hertz <vincent.hertz@gmail.com>
2024-03-24 21:58:29 +0100

    maj Route Error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Error from "./pages/Error";
+// import Error from "./pages/Error";
 import "./styles/App.scss";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="*" element={<Error />} />
+        {/* <Route path="*" element={<Error />} /> */}
       </Routes>
     </BrowserRouter>
   );
