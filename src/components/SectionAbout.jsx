df133a2a63265620258665763be052893a23fa39
Vincent Hertz <vincent.hertz@gmail.com>
2024-03-25 13:35:27 +0100

    maj texte et css

--- src/components/SectionAbout.jsx
+++ src/components/SectionAbout.jsx
@@ -8,10 +8,10 @@ function SectionAbout() {
       <p>
         Passionné par le codage et les technologies web, j’ai décidé d’apprendre
         la programmation, suite à une première expérience dans les media. Après
-        plusieurs mois de formations et l’optention d’un diplôme, j’ai acquis de
-        solides compétences techniques en JavaScript, HTML, CSS, ainsi qu’avec
-        des frameworks et bibliothèques comme React.js et Node.js, au travers
-        d’une variété de projets réalisés.
+        plusieurs mois de formations et l’obtention d’un diplôme OpenClassrooms,
+        j’ai acquis de solides compétences techniques en JavaScript, HTML, CSS,
+        ainsi qu’avec des frameworks et bibliothèques comme React.js et Node.js,
+        au travers d’une variété de projets réalisés.
       </p>
     </section>
   );
