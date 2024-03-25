b6fdcdd6220c0cc9b55e228c29c59bee9b0b4389
Vincent Hertz <vincent.hertz@gmail.com>
2024-03-25 13:07:21 +0100

    modif texte

--- src/components/SectionAbout.jsx
+++ src/components/SectionAbout.jsx
@@ -6,12 +6,12 @@ function SectionAbout() {
     <section id="about" className="section__about">
       <h2>A propos de moi</h2>
       <p>
-        Passionné par le codage et le technologies web, j’ai décidé d’apprendre
+        Passionné par le codage et les technologies web, j’ai décidé d’apprendre
         la programmation, suite à une première expérience dans les media. Après
-        plusieurs mois de formations et l’optention d’un diplôme dans le
-        domaine, j’ai acquis de solides compétences techniques en JavaScript,
-        HTML, CSS, ainsi qu’avec des frameworks et bibliothèques populaires
-        comme React.js et Node.js, au travers d’une variété de projets réalisés.
+        plusieurs mois de formations et l’optention d’un diplôme, j’ai acquis de
+        solides compétences techniques en JavaScript, HTML, CSS, ainsi qu’avec
+        des frameworks et bibliothèques comme React.js et Node.js, au travers
+        d’une variété de projets réalisés.
       </p>
     </section>
   );
